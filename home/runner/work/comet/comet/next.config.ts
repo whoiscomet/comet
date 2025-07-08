21e5f984dcdcd658ebff33d35f998bc0d96bb208
Comet <workwithcomet@gmail.com>
2025-07-08 10:07:11 +0000

    Commit and push the code

--- home/runner/work/comet/comet/next.config.ts
+++ home/runner/work/comet/comet/next.config.ts
@@ -3,7 +3,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/comet',
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
