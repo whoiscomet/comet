2fc424cfca64ab0f0d8e22e6b1da3173f89a1f4a
Comet <workwithcomet@gmail.com>
2025-07-08 17:38:18 +0530

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,5 +1,4 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
+
 {pkgs}: {
   # Which nixpkgs channel to use.
   channel = "stable-24.11"; # or "unstable"
@@ -10,8 +9,6 @@
   ];
   # Sets environment variables in the workspace
   env = {};
-  # This adds a file watcher to startup the firebase emulators. The emulators will only start if
-  # a firebase.json file is written into the user's directory
   services.firebase.emulators = {
     detect = true;
     projectId = "demo-app";
